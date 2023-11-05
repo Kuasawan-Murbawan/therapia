3c586a126e49f7fb4d849133fd801b3415fd7ba2
Kuasawan-Murbawan <davidafiq@gmail.com>
2023-11-06 00:36:46 +0800

    creating customer profile ui

---
+++ app/src/main/java/com/husyairi/therapia/CustomerProfile.java
@@ -0,0 +1,14 @@
+package com.husyairi.therapia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_profile);
+    }
+}
