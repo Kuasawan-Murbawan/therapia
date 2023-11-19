75f1edceaeb489c41f48b67f709f79b7f0d8c262
Kuasawan-Murbawan <davidafiq@gmail.com>
2023-11-19 19:28:10 +0800

    patient homepage

--- app/src/main/java/com/husyairi/therapia/Login.java
+++ app/src/main/java/com/husyairi/therapia/Login.java
@@ -2,13 +2,33 @@ package com.husyairi.therapia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, CustomerProfile.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/husyairi/therapia/PatientHomepage.java
@@ -0,0 +1,14 @@
+package com.husyairi.therapia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientHomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_homepage);
+    }
+}
