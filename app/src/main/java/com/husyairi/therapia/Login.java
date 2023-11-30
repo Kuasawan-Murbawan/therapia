d4733a0495192861731be4839e35cf3832b99342
Kuasawan-Murbawan <davidafiq@gmail.com>
2023-11-30 23:08:06 +0800

    create routing

--- app/src/main/java/com/husyairi/therapia/Login.java
+++ app/src/main/java/com/husyairi/therapia/Login.java
@@ -23,7 +23,7 @@ public class Login extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this, CustomerProfile.class);
+                Intent intent = new Intent(Login.this, PatientHomepage.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/husyairi/therapia/PatientHomepage.java
+++ app/src/main/java/com/husyairi/therapia/PatientHomepage.java
@@ -2,13 +2,53 @@ package com.husyairi.therapia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class PatientHomepage extends AppCompatActivity {
+public class PatientHomepage extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView profile_icon_navbar;
+    private ImageView booking_icon_navbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_homepage);
+
+        profile_icon_navbar = findViewById(R.id.profile_icon_navbar);
+        profile_icon_navbar.setOnClickListener(this);
+
+        booking_icon_navbar = findViewById(R.id.booking_icon_navbar);
+        booking_icon_navbar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        switch(view.getId()) {
+            case R.id.profile_icon_navbar:
+                intent = new Intent(this, CustomerProfile.class);
+                break;
+
+            case R.id.booking_icon_navbar:
+                intent = new Intent(this, Booking.class);
+                break;
+
+            default:
+                intent = null;
+        }
+
+        if(intent != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
